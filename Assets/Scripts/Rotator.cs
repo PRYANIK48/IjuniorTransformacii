980c879baaa7b27c4dc0d0993b7c3cdb197e18fc
PRYANIK48 <pryanik481@gmail.com>
2024-11-01 16:16:26 +0300

    s

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 1.0f;
+
     private void Update()
     {
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class Scaler : MonoBehaviour
 {
     [SerializeField] private float scaleSpeed = 1.0f;
+
     private void Update()
     {
-        transform.localScale += new Vector3(1,1,1) * scaleSpeed * Time.deltaTime;
+        transform.localScale += Vector3.one * scaleSpeed * Time.deltaTime;
     }
 }
